9af7319651b4baf76162470e0648eb27c8abdf6e
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-09-02 23:53:02 +0100

    noms joueurs fait

---
+++ src/players.js
@@ -0,0 +1,40 @@
+//array of JSon objects, containing the players details
+
+const players = [
+  {
+    "name": 'Ronaldo',
+    "team": 'Real Madrid',
+    "nationality": 'Portuguais',
+    "jerseyNumber": '7',
+    "age": 25,
+    "image": 'https://upload.wikimedia.org/wikipedia/commons/2/23/Cristiano_Ronaldo_WC2022_-_01.jpg'
+  },
+
+   {
+    "name": 'Zidane',
+    "team": 'Real Madrid',
+    "nationality": 'Français',
+    "jerseyNumber": '10',
+    "age": 29,
+    "image":'https://www.realmadrid.com/img/vertical_380px/zidane_20220712070022.jpg'
+  },
+
+   {
+    "name": 'Messi',
+    "team": 'Barca',
+    "nationality": 'Argentin',
+    "jerseyNumber": '09',
+    "age": 27,
+    "image": 'https://cnnespanol.cnn.com/wp-content/uploads/2023/04/230216150618-lionel-messi-barcelona-photo-getty-deportes-full-169.jpg?quality=100&strip=info'
+  },
+
+   {
+    "name": 'Ronaldinho',
+    "team": 'Barca',
+    "nationality": 'Bresilien',
+    "jerseyNumber": '08',
+    "age": 25,
+    "image": 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSui8dq97SlzMWjn4UBFhRUGcgj1suQo4WKSRrObJB1&s'
+   }
+
+]
