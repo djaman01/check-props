710a4f04e9685f34fdf1c70b4f2bbe4e325e5a3f
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-09-03 20:51:43 +0100

    ajout grid

--- src/PlayersList.js
+++ src/PlayersList.js
@@ -3,7 +3,7 @@ import Player from './Player.js'
 
 export default function PlayersList() {
   return (
-    <div>
+    <div className='grid-container'>
       {/* On pass au player component plusieurs props qui sont les valeurs de chaque objet de l'array players */}
       {players.map(e => <Player {...e}/>)}
     </div>
