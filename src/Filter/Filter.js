cdcf4ee2f0aac96112990a1d7d853f8c01bb7666
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-09-10 17:43:53 +0100

    1ere utilisation useEffect

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { players } from "../players";
 import './filter.css';
 import Player from "../Player";
@@ -11,6 +11,14 @@ export default function Filter() {
 
   const filteredArray = players.filter((element,index)=>element.name.toLowerCase().includes(name.toLowerCase())) ;
 
+  //using the useEffect hook to perform action after the component has rendered or when certain dependencies have changed
+  //A dependecy is the state variable that the sideEffect function relies on to be executed
+
+  //Le 1er parametre de useEffect est la function qui provoque/handle l'effet
+  //Le 2eme parametre est une array: si vide, alors le side effect aura lieu on the first render and that's all
+  //If we add state variable inside the array, we call them dependencies, and the useEffect will happen on the first render and when there will be a change on the state
+
+  useEffect(()=>{document.title= `Players page`},[])
 
   return (
     <>
