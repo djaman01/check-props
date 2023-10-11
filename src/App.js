42c0d5db22e3a36008699e5069ade8cafd509a57
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-10-12 00:45:39 +0100

    fin link front and back

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import Carousel from './Slider';
-import './App.css';
-import Filter from './Filter/Filter';
-import Footer from './Footer';
-import PlayersList from './PlayersList';
-import Player from './Player';
-
-
-
-
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
- return (
-  <>
-    <h1 className="text-3xl font-bold underlin text-red-500">
-      Hello world!
-    </h1>
-    {/* <Carousel /> */}
-  <Filter />
-  {/* <Footer /> */}
+  const [formData, setFormData] = useState({
+    Nom: '',
+    Prenom: '',
+  });
 
-  </>
-  
- )
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:3005/', formData);
+      // Handle the response if needed (e.g., show a success message)
+      console.log('Response from the server:', response.data);
+    } catch (error) {
+      // Handle the error (e.g., show an error message)
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underlin text-red-500">Hello world!</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="Nom">Nom:</label>
+          <input
+            type="text"
+            id="Nom"
+            name="Nom"
+            value={formData.Nom}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="Prenom">Prenom:</label>
+          <input
+            type="text"
+            id="Prenom"
+            name="Prenom"
+            value={formData.Prenom}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
